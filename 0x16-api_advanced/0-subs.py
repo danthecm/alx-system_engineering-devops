204af9b5ba78e23e08df2199aed0a1c124e7e36a
danthecm <etchiedaniel1@gmail.com>
2023-06-07 22:17:00 +0100

    feat: working on subrredit query

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module containing a function to get number of subscribes"""
+
+
+def number_of_subscribers(subreddit):
+    """
+    Fetch the total number of subscribers from reddit api
+    for a given subreddit
+    """
+    pass
