0d3646455f03d0a464ea0ef9f1ac976d9481c2c2
danthecm <etchiedaniel1@gmail.com>
2023-08-19 16:14:08 +0100

    1: 0x16 task 2: recursive

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Modue that contains a function recurse that recursively calls Reeddit Api for titles
+'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], next_page=None, count=0):
+    '''
+    Queries Reddit Api for recursively for titles
+    Args:
+        subreddit(str) - The name of the subreddit to check
+        hot_list - List to append
+        next_page - Page to be passed in parameter
+        count - Counter
+    '''
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # if page specified, pass as parameter
+    if next_page:
+        url += '?after={}'.format(next_page)
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    data = r.json()['data']
+    posts = data['children']
+    for post in posts:
+        count += 1
+        hot_list.append(post['data']['title'])
+
+    next_page = data['after']
+    if next_page is not None:
+        return recurse(subreddit, hot_list, next_page, count)
+    else:
+        return hot_list
